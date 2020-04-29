d2cbda2a48fd1657efc16ce9eb81d6208d311df9
sakaguchi-yuki <sakaguchiyuki@team-lab.com>
2020-04-29 23:58:13 +0900

    First commit

---
+++ src/scripts/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <p>
+      <img class="logo" src="../images/logo.jpg" alt="ロゴ">
+      <span class="sample">テスト</span>
+    </p>
+    <MyComponent :message="$data.message" />
+    <form @submit="onSubmit">
+      <input v-model="$data.text" type="text">
+      <button type="submit">送信</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import socket from './utils/socket';
+
+// components
+import MyComponent from './components/MyComponent.vue';
+
+export default Vue.extend({
+  components: {
+    MyComponent
+  },
+  data() {
+    return {
+      message: '',
+      text: ''
+    };
+  },
+  created() {
+    socket.on('connect', () => {
+      console.log('connected!');
+    });
+
+    socket.on('send', (message) => {
+      console.log(message);
+      this.$data.message = message;
+    });
+  },
+  methods: {
+    /**
+     * Enterボタンを押したとき
+     */
+    onSubmit(e) {
+      e.preventDefault();
+      socket.emit('send', this.$data.text);
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  width: 40px;
+}
+
+.sample {
+  color: $red;
+}
+</style>
